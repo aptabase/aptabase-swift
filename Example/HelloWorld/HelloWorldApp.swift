4ddcc37615c621dde54dab6cacca840692221dc8
goenning <me@goenning.net>
2023-04-03 12:27:38 +0100

    example app

---
+++ Example/HelloWorld/HelloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldApp.swift
+//  HelloWorld
+//
+//  Created by Guilherme Oenning on 03/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
